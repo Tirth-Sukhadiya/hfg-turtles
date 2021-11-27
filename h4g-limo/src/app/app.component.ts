40d96d0b987166edd1d0c773f010e0f494942b50
Abhinav Aggarwal <abhinav.aggarwal@tmspl.com>
2021-11-27 19:08:30 +0530

    heatmap working but colors not proper

--- h4g-limo/src/app/app.component.ts
+++ h4g-limo/src/app/app.component.ts
@@ -4,6 +4,7 @@ import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import EsriConfig from "@arcgis/core/config";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import * as heatmapRendererCreator from "@arcgis/core/smartMapping/renderers/heatmap";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -36,14 +37,29 @@ export class AppComponent implements OnInit {
           center: [4.288788, 52.078663], // Longitude, latitude
           zoom: 11, // Zoom level
           container: this.mapDiv // Div element,
-
         });
 
         //Trailheads feature layer (points)
         const layer = new FeatureLayer({
           url: "https://services3.arcgis.com/jNF04dtssnue8VcP/arcgis/rest/services/chemicals_measurepoints/FeatureServer/0"
         });
+
+        let heatmapParams: any = {
+          layer: layer,
+          view: this.view,
+          field: "Numeriekew"
+        };
+
+        heatmapRendererCreator.createRenderer(heatmapParams).then(function (response: any) {
+          console.log(response);
+          // Reset the minimum of the statistics as the heatmap will define color range linearly between
+          // min and max
+          // response.statistics.min = response.statistics.max*0.8;
+          layer.renderer = response.renderer;
+        });
+
         this.map.add(layer);
+
       }
     }, 300);
   }
