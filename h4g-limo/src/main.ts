a59813276a991771d2def0984d6797fe80c98be4
Abhinav Aggarwal <abhinav.aggarwal@tmspl.com>
2021-11-27 16:44:23 +0530

    calcite components working properly

--- h4g-limo/src/main.ts
+++ h4g-limo/src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { defineCustomElements } from "@esri/calcite-components/dist/loader";
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -8,5 +8,8 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
+
+defineCustomElements(window, {
+  resourcesUrl: "https://js.arcgis.com/calcite-components/1.0.0-beta.69/assets"
+});
