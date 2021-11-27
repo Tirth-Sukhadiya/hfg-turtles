daffa07312e3f557ff3901d486c624eb4372b876
Tirth Sukhadiya <tirth.sukhadiya@tmspl.com>
2021-11-28 05:25:46 +0530

    Layer and chemical switch, popuptemplate, search widget, legends, and compass widget

--- h4g-limo/src/app/app.component.ts
+++ h4g-limo/src/app/app.component.ts
@@ -4,11 +4,46 @@ import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import EsriConfig from "@arcgis/core/config";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
-import * as heatmapRendererCreator from "@arcgis/core/smartMapping/renderers/heatmap";
+import { HttpClient } from '@angular/common/http';
+import Search from "@arcgis/core/widgets/Search";
+import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer";
+import SimpleLineSymbol from "@arcgis/core/symbols/SimpleLineSymbol";
+import SimpleFillSymbol from "@arcgis/core/symbols/SimpleFillSymbol";
+import Legend from "@arcgis/core/widgets/Legend";
+import Compass from '@arcgis/core/widgets/Compass';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+interface Legends {
+  id: number;
+  name: string;
+  isChecked: boolean;
+}
+
+enum MapLayerType {
+  Chemicals,
+  ProtectedAreas,
+  Municipalities,
+  GreenHouses
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':leave',
+          [
+            animate('1s ease-in',
+              style({ height: 0, opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class AppComponent implements OnInit {
   title = 'Limo';
@@ -17,33 +52,20 @@ export class AppComponent implements OnInit {
   mapDiv: HTMLDivElement | undefined;
   layer: FeatureLayer;
 
-  chemicalList = [
-    {
-      viewValue: "ammoniak",
-      value: "ammoniak"
-    },
-    {
-      viewValue: "acenafteen",
-      value: "acenafteen"
-    },
-    {
-      viewValue: "ammonium",
-      value: "ammonium"
-    },
-    {
-      viewValue: "chlorofyl-a",
-      value: "chlorofyl-a"
-    },
-    {
-      viewValue: "koper",
-      value: "koper"
-    },
-    {
-      viewValue: "zink",
-      value: "zink"
-    }
+  checmicalLayer: FeatureLayer;
+  protectedAreaLayer: FeatureLayer;
+  municipalityLayer: FeatureLayer;
+  greenHouseLayer: FeatureLayer;
+  showLandingBanner: boolean = true;
+
+  LegendsData: Legends[] = [
+    { id: 0, name: 'Chemicals', isChecked: true },
+    { id: 1, name: 'Protected Areas', isChecked: true },
+    { id: 2, name: 'Municipalities', isChecked: true },
+    { id: 3, name: 'Green Houses', isChecked: true }
   ];
 
+  chemicalList = [];
   monthList = [
     {
       viewValue: "JAN - 2019",
@@ -51,10 +73,18 @@ export class AppComponent implements OnInit {
     }
   ];
 
-  constructor() {
+  constructor(private http: HttpClient) {
+    this.http.get("./assets/chemicals.json").subscribe((_chemList: any) => {
+      this.chemicalList = _chemList;
+    });
   }
 
   ngOnInit() {
+
+    setTimeout(() => {
+      this.showLandingBanner = false;
+    }, 8000);
+
     EsriConfig.apiKey = environment.esriConfigApiKey;
 
     const mapDivInterval = setInterval(() => {
@@ -74,16 +104,45 @@ export class AppComponent implements OnInit {
           container: this.mapDiv // Div element,
         });
 
-        //Trailheads feature layer (points)
-        this.layer = new FeatureLayer({
-          url: "https://services3.arcgis.com/jNF04dtssnue8VcP/arcgis/rest/services/chemicals_measurepoints/FeatureServer/0",
-          outFields: ["Paramete_2", "Paramete_3", "MonsterC_1"],
-          featureReduction: {
-            type: "cluster"
-          },
+        //#region Add Compass to view
+        let compass = new Compass({
+          view: this.view
+        });
+        this.view.ui.add(compass, "top-left");
+        //#endregion
+
+        //#region Search Widget
+        const searchWidget = new Search({
+          view: this.view
         });
 
-        this.map.add(this.layer);
+        // Add the search widget to the top right corner of the view
+        this.view.ui.add(searchWidget, {
+          position: "top-left"
+        });
+        //#endregion
+
+        //#region Chemical Layer
+        this.checmicalLayer = this.GetLayer(MapLayerType.Chemicals);
+        this.map.add(this.checmicalLayer, MapLayerType.Chemicals);
+        //#endregion
+
+        //#region Protected Areas Layer
+        this.protectedAreaLayer = this.GetLayer(MapLayerType.ProtectedAreas);
+        this.map.add(this.protectedAreaLayer, MapLayerType.ProtectedAreas);
+        //#endregion
+
+        //#region Protected Areas Layer
+        this.municipalityLayer = this.GetLayer(MapLayerType.Municipalities);
+        this.map.add(this.municipalityLayer, MapLayerType.Municipalities);
+        //#endregion
+
+        //#region Green house Layer
+        this.greenHouseLayer = this.GetLayer(MapLayerType.GreenHouses);
+        this.map.add(this.greenHouseLayer, MapLayerType.GreenHouses);
+        //#endregion
+
+        //#region Filter for Chemical Layer
         var filter = document.getElementById("chemicalFilter");
         // filters the layer using a definitionExpression
         // based on a religion selected by the user
@@ -91,9 +150,35 @@ export class AppComponent implements OnInit {
           var newValue = evt.target.value;
           this.updateDefinitionExpression(newValue);
         });
+        //#endregion
 
+        //#region Legends
+        const legend = new Legend({
+          view: this.view,
+          layerInfos: [
+            {
+              layer: this.greenHouseLayer,
+              title: "Green Houses"
+            },
+            {
+              layer: this.municipalityLayer,
+              title: "Municipalities"
+            },
+            {
+              layer: this.protectedAreaLayer,
+              title: "Protected Areas"
+            },
+            {
+              layer: this.checmicalLayer,
+              title: "Chemicals Measurement"
+            }
+          ]
+        });
 
-        //#region Abhinav - Heatmap
+        this.view.ui.add(legend, "bottom-left");
+        //#endregion
+
+        //#region Heatmap
         // let heatmapParams: any = {
         //   layer: layer,
         //   view: this.view,
@@ -115,10 +200,101 @@ export class AppComponent implements OnInit {
   }
 
   updateDefinitionExpression(value) {
-    // var definitionExpression = value ? "Paramete_2 = '" + value + "'" : null;
-    var definitionExpression = value ? `Paramete_2 = '${value}'` : null;
-    this.layer.definitionExpression = definitionExpression;
-    // (<any>this.layer).setDefinitionExpression(definitionExpression);
+    let definitionExpression = value ? `Paramete_2 = '${value}'` : null;
+    this.checmicalLayer.definitionExpression = definitionExpression;
     // (<any>this.map).infoWindow.hide();
   }
+
+  onChange(event, id: number) {
+    if (event.currentTarget.checked) {
+      this.map.layers.add(this.GetLayer(id), id);
+    } else {
+      this.map.layers.removeAt(id);
+    }
+  }
+
+  GetLayer(EnumMapLayerType: MapLayerType): FeatureLayer {
+    let retlayer: any;
+    switch (EnumMapLayerType) {
+      case MapLayerType.Chemicals:
+        retlayer = new FeatureLayer({
+          url: "https://services3.arcgis.com/jNF04dtssnue8VcP/arcgis/rest/services/measure_locations_shortv/FeatureServer/0",
+          outFields: ["Paramete_2", "Paramete_3", "MonsterC_1"],
+          popupTemplate: {
+            title: "{Paramete_2} - {Paramete_1}",
+            content: [
+              {
+                type: "fields",
+                fieldInfos: [
+                  {
+                    fieldName: "Paramete_3",
+                    label: "Chemical Bond"
+                  },
+                  {
+                    fieldName: "MonsterC_1",
+                    label: "Measurement taken from"
+                  },
+                  {
+                    fieldName: "Resultaatd",
+                    label: "Result Date"
+                  },
+                  {
+                    fieldName: "Numeriekew",
+                    label: "Value"
+                  }
+                ]
+              }
+            ]
+          }
+          // featureReduction: {
+          //   type: "cluster"
+          // },
+        });
+        break;
+      case MapLayerType.ProtectedAreas:
+        const renderer = new SimpleRenderer({
+          symbol: new SimpleLineSymbol({
+            width: 1.5,
+            color: "#00CDEF",
+            style: 'short-dash-dot'
+          }),
+        });
+        retlayer = new FeatureLayer({
+          url: "https://services3.arcgis.com/jNF04dtssnue8VcP/arcgis/rest/services/protected_areas/FeatureServer/0",
+          renderer: renderer,
+          opacity: 20
+        });
+        break;
+      case MapLayerType.Municipalities:
+        const mrenderer = new SimpleRenderer({
+          symbol: new SimpleLineSymbol({
+            width: 1,
+            color: "#A7586A",
+            style: "solid"
+          })
+        });
+
+        retlayer = new FeatureLayer({
+          url: "https://services3.arcgis.com/jNF04dtssnue8VcP/arcgis/rest/services/municipalities/FeatureServer/0",
+          renderer: mrenderer
+        });
+        break;
+      case MapLayerType.GreenHouses:
+        const Grenderer = new SimpleRenderer({
+          symbol: new SimpleFillSymbol({
+            color: "#81C784"
+          })
+        });
+
+        retlayer = new FeatureLayer({
+          url: "https://services3.arcgis.com/jNF04dtssnue8VcP/arcgis/rest/services/greenhouses_data/FeatureServer/0",
+          renderer: Grenderer,
+          geometryType: "polygon",
+        });
+        break;
+    }
+    return retlayer;
+  }
+
+
 }
--- h4g-limo/src/app/app.module.ts
+++ h4g-limo/src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
