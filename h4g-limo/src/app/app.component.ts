0b96afee70c0950ae2e1f1032c4a7f4c79be1764
Tirth Sukhadiya <tirth.sukhadiya@tmspl.com>
2021-11-27 20:07:15 +0530

    Merge abhinav changes for heatmap and comment it out for now

--- h4g-limo/src/app/app.component.ts
+++ h4g-limo/src/app/app.component.ts
@@ -15,6 +15,41 @@ export class AppComponent implements OnInit {
   map: Map | undefined;
   view: MapView | undefined;
   mapDiv: HTMLDivElement | undefined;
+  layer: FeatureLayer;
+
+  chemicalList = [
+    {
+      viewValue: "ammoniak",
+      value: "ammoniak"
+    },
+    {
+      viewValue: "acenafteen",
+      value: "acenafteen"
+    },
+    {
+      viewValue: "ammonium",
+      value: "ammonium"
+    },
+    {
+      viewValue: "chlorofyl-a",
+      value: "chlorofyl-a"
+    },
+    {
+      viewValue: "koper",
+      value: "koper"
+    },
+    {
+      viewValue: "zink",
+      value: "zink"
+    }
+  ];
+
+  monthList = [
+    {
+      viewValue: "JAN - 2019",
+      value: 1
+    }
+  ];
 
   constructor() {
   }
@@ -40,27 +75,50 @@ export class AppComponent implements OnInit {
         });
 
         //Trailheads feature layer (points)
-        const layer = new FeatureLayer({
-          url: "https://services3.arcgis.com/jNF04dtssnue8VcP/arcgis/rest/services/chemicals_measurepoints/FeatureServer/0"
+        this.layer = new FeatureLayer({
+          url: "https://services3.arcgis.com/jNF04dtssnue8VcP/arcgis/rest/services/chemicals_measurepoints/FeatureServer/0",
+          outFields: ["Paramete_2", "Paramete_3", "MonsterC_1"],
+          featureReduction: {
+            type: "cluster"
+          },
         });
 
-        let heatmapParams: any = {
-          layer: layer,
-          view: this.view,
-          field: "Numeriekew"
-        };
-
-        heatmapRendererCreator.createRenderer(heatmapParams).then(function (response: any) {
-          console.log(response);
-          // Reset the minimum of the statistics as the heatmap will define color range linearly between
-          // min and max
-          // response.statistics.min = response.statistics.max*0.8;
-          layer.renderer = response.renderer;
+        this.map.add(this.layer);
+        var filter = document.getElementById("chemicalFilter");
+        // filters the layer using a definitionExpression
+        // based on a religion selected by the user
+        filter.addEventListener("change", (evt: any) => {
+          var newValue = evt.target.value;
+          this.updateDefinitionExpression(newValue);
         });
 
-        this.map.add(layer);
 
+        //#region Abhinav - Heatmap
+        // let heatmapParams: any = {
+        //   layer: layer,
+        //   view: this.view,
+        //   field: "Numeriekew"
+        // };
+
+        // heatmapRendererCreator.createRenderer(heatmapParams).then(function (response: any) {
+        //   console.log(response);
+        //   // Reset the minimum of the statistics as the heatmap will define color range linearly between
+        //   // min and max
+        //   // response.statistics.min = response.statistics.max*0.8;
+        //   layer.renderer = response.renderer;
+        // });
+
+        // this.map.add(layer);
+        //#endregion
       }
     }, 300);
+  }
+
+  updateDefinitionExpression(value) {
+    // var definitionExpression = value ? "Paramete_2 = '" + value + "'" : null;
+    var definitionExpression = value ? `Paramete_2 = '${value}'` : null;
+    this.layer.definitionExpression = definitionExpression;
+    // (<any>this.layer).setDefinitionExpression(definitionExpression);
+    // (<any>this.map).infoWindow.hide();
   }
 }
