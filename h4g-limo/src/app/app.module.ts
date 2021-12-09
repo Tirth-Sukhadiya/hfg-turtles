5e51c3b11b54010806057af576100c00d82d9fc9
Abhinav Aggarwal <abhinav.aggarwal@tmspl.com>
2021-12-10 02:21:06 +0530

    login screen added, loader added and some other things

--- h4g-limo/src/app/app.module.ts
+++ h4g-limo/src/app/app.module.ts
@@ -5,16 +5,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FullLoaderComponent } from './components/full-loader/full-loader.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { SkeletonElementsModule } from 'skeleton-elements/angular';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    FullLoaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    CarouselModule.forRoot(),
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatFormFieldModule,
+    MatInputModule,
+    SkeletonElementsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent],
